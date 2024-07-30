391a26e43818c61a4169ee71227081972237bfec
Deepak Adhikari <itprofessional.deepakkumar@gmail.com>
2024-07-30 18:20:29 -0400

    [Feature] Variable Title added

--- HomeView.swift
+++ HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var title : String = "Hello World!"
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("Second File")
