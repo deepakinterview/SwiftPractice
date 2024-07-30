2af90aedecb776272b99956af0addeab0d8328f2
Deepak Adhikari <itprofessional.deepakkumar@gmail.com>
2024-07-30 16:18:52 -0400

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Deepak Kumar on 2024-07-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
