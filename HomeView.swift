b7099630255fb613323cd935104d3b6e1f5a4937
Deepak Adhikari <itprofessional.deepakkumar@gmail.com>
2024-07-30 17:33:57 -0400

    Add HomeView

---
+++ HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Deepak Kumar on 2024-07-30.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Second File")
+    }
+}
+
+#Preview {
+    HomeView()
+}
