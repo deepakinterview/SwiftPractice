a500deb1237feef915ef2f32143cce917fe6a430
Deepak Adhikari <itprofessional.deepakkumar@gmail.com>
2024-07-30 18:59:35 -0400

    [Feature] Text (Third File) added in Home View File

--- HomeView.swift
+++ HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("Second File")
+        Text("Third File")
     }
 }
 
