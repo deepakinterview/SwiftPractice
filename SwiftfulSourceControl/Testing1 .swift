7f8a1386900de3ee850089f0cef0b6ef6b610456
Deepak Adhikari <itprofessional.deepakkumar@gmail.com>
2024-07-31 15:43:46 -0400

    Two New Swift File Created

---
+++ SwiftfulSourceControl/Testing1 .swift
@@ -0,0 +1,19 @@
+//
+//  Testing1 .swift
+//  SwiftfulSourceControl
+//
+//  Created by Deepak Kumar on 2024-07-31.
+//
+
+import SwiftUI
+
+struct Testing1_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("New Swift File Created")
+    }
+}
+
+#Preview {
+    Testing1_()
+}
